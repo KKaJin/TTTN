9ae5a3d203d9ae417e492d8e0af641b43bb8309d
KKaJin <161930254+KKaJin@users.noreply.github.com>
2025-07-30 09:46:49 +0700

    Add files via upload

--- routes/search.py
+++ routes/search.py
@@ -7,22 +7,15 @@ bp = Blueprint('search', __name__)
 def search():
     query = request.args.get('query')
     url = f'https://nominatim.openstreetmap.org/search?q={query}&format=json&limit=5'
-    headers = {
-        'User-Agent': 'Ban do hanh chinh'
-    }
+    headers = {'User-Agent': 'Ban do hanh chinh'}
+    r = requests.get(url ,headers=headers) 
+    data = r.json()
 
-    try:
-        r = requests.get(url ,headers=headers) 
-        r.raise_for_status()
-        data = r.json()
-
-        results = [{
-            'name': d['display_name'],
-            'lat': float(d['lat']),
-            'lng': float(d['lon'])
-        } for d in data]
-
-        return jsonify(results)
-
-    except requests.exceptions.RequestException as e:
-        return jsonify({'error': str(e)})
+    results = [{
+        'name': d['display_name'],
+        'lat': float(d['lat']),
+        'lng': float(d['lon']),
+        'type': d.get('type'),
+        'display_name': d.get('display_name')
+    } for d in data]
+    return jsonify(results)
