4fcdb75d69e03ee9fa0a879b4b006b79d5ff20d0
KKaJin <161930254+KKaJin@users.noreply.github.com>
2025-08-08 17:54:04 +0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-PROVINCE_DIR = os.path.join(BASE_DIR, 'data', 'geojson', 'provinces')
-COMMUNE_DIR = os.path.join(BASE_DIR, 'data', 'geojson', 'communes')
-
-ORS_KEY = 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjcyMmFlZmJkOGJkYzRiMjA4OTYwMmNjNjg2NjZjZjJjIiwiaCI6Im11cm11cjY0In0='
-ORS_URL = 'https://api.openrouteservice.org/v2/directions/driving-car'
